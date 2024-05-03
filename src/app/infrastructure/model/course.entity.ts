3e3732082651e02441d134520a5399c878fbed1a
Frida-Dam <u202218352@upc.edu.pe>
2024-05-03 18:30:33 -0500

    feat: Added components, pages and the configuration

---
+++ src/app/infrastructure/model/course.entity.ts
@@ -0,0 +1,20 @@
+export class Course {
+  "room": string;
+  "course": string;
+  "start_time": string;
+  "end_time": string;
+  "days": string;
+  "teacher": string;
+  "image_url": string;
+
+  constructor() {
+    this.room = "";
+    this.course = "";
+    this.start_time = "";
+    this.end_time = "";
+    this.days = "";
+    this.teacher = "";
+    this.image_url = "";
+  }
+}
+
---
+++ src/app/infrastructure/pages/classroom-students/classroom-students.component.ts
@@ -0,0 +1,28 @@
+// infrastructure/pages/classroom-capacity/classroom-capacity.component.ts
+import { Component, OnInit } from '@angular/core';
+import {StudentsService} from "../../services/students.service";
+import {Student} from "../../model/student.entity";
+
+@Component({
+  selector: 'app-classroom-students',
+  templateUrl: './classroom-students.component.html',
+  styleUrls: ['./classroom-students.component.css']
+})
+export class ClassroomStudentsComponent implements OnInit {
+  dataSource: Student[] = [];
+  displayedColumns: string[] = ['id', 'name', 'delete'];
+
+  constructor(private studentService: StudentsService) {}
+
+  ngOnInit() {
+    this.studentService.getAll().subscribe({
+      next: (data) => {
+        this.dataSource = data;
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+
+
+}
