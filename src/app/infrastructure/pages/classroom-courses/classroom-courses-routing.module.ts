607f5eb7e14a67f3274e2a5699ee653aee039d9e
Frida-Dam <u202218352@upc.edu.pe>
2024-06-20 14:50:38 -0500

    fix: Fixed classroom-courses page

--- src/app/infrastructure/pages/classroom-courses/classroom-courses-routing.module.ts
+++ src/app/infrastructure/pages/classroom-courses/classroom-courses-routing.module.ts
@@ -5,6 +5,16 @@ import {LayoutComponent} from "../../../shared/layout/layout.component";
 import {ClassroomCoursesComponent} from "./classroom-courses.component";
 import { CoursesViewComponent } from '../courses-view/courses-view.component';
 import { CourseDetailComponent } from '../course-detail/course-detail.component';
+import {ClassroomsHomeComponent} from "../../../features/classrooms/classrooms-home/classrooms-home.component";
+import {
+    ClassroomsCoursesComponent
+} from "../../../features/classrooms/classrooms-home/classrooms-courses/classrooms-courses.component";
+import {
+    ClassroomsAttendanceComponent
+} from "../../../features/classrooms/classrooms-home/classrooms-courses/classrooms-attendance/classrooms-attendance.component";
+import {
+    ClassroomGradesComponent
+} from "../../../features/classrooms/classrooms-home/classrooms-courses/classrooms-grades/classroom-grades.component";
 
 
 const routes: Routes =[
@@ -13,12 +23,14 @@ const routes: Routes =[
         component: LayoutComponent,
         children:[
             {path:'', component: ClassroomCoursesComponent},
+            { path: '', redirectTo: '/classroom-courses', pathMatch: 'full' },
+            { path: 'courses-view', component: CoursesViewComponent },
+            { path: 'course-detail/:id', component: CourseDetailComponent },
         ]
     },
-    { path: '', redirectTo: '/classroom-courses', pathMatch: 'full' },
-    { path: 'courses-view', component: CoursesViewComponent },
-    { path: 'course-detail/:id', component: CourseDetailComponent },
+
 ];
+
 @NgModule({
     imports:[RouterModule.forChild(routes)],
     exports:[RouterModule]
